9040167502bc34b49e67d841046e2a67c3ba8ca1
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-21 15:42:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ string) {
   return("I can't hear you!");
 } else if ((string.toUpperCase()) === string) {
   return("YES INDEED!");
-} else if ((string()) === "I love you, Grandma.") {
+} else if ((string) === "I love you, Grandma.") {
   return("I love you, too.");
 }
 }
