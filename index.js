3ad1f084c995550c0209167006133075edc32bf6
Barbara Dixon <babsnwosisi@yahoo.com>
2017-09-21 15:40:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
 function sayHiToGrandma(string) {
   if ((string.toLowerCase()) ===
 string) {
-  console.log("I can't hear you!");
+  return("I can't hear you!");
 } else if ((string.toUpperCase()) === string) {
-  console.log("YES INDEED!");
+  return("YES INDEED!");
 } else if ((string()) === "I love you, Grandma.") {
-  console.log("I love you, too.");
+  return("I love you, too.");
 }
 }
